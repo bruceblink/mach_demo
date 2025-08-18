4f0da8ae4cc99719952c25cd3547750694f7d89b
likanug <likanug.g@qq.com>
2025-08-18 09:53:34 +0800

    更新build.zig，现在支持从examples中运行代码

--- build.zig
+++ build.zig
@@ -4,43 +4,56 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    // Create our Mach app module, where all our code lives.
-    const app_mod = b.createModule(.{
-        .root_source_file = b.path("src/App.zig"),
-        .optimize = optimize,
-        .target = target,
-    });
+    const examples = [_]struct {
+        file: []const u8,
+        name: []const u8,
+        libc: bool = false,
+    }{
+        .{ .file = "examples/01_piano/App.zig", .name = "example_1" },
+        .{ .file = "examples/02_core-triangle/App.zig", .name = "example_2" },
+    };
 
-    // Add Mach import to our app.
-    const mach_dep = b.dependency("mach", .{
-        .target = target,
-        .optimize = optimize,
-    });
-    app_mod.addImport("mach", mach_dep.module("mach"));
+    {
+        for (examples) |ex| {
+            // Create our Mach app module, where all our code lives.
+            const app_mod = b.createModule(.{
+                .root_source_file = b.path(ex.file),
+                .optimize = optimize,
+                .target = target,
+            });
 
-    // Have Mach create the executable for us
-    const exe = @import("mach").addExecutable(mach_dep.builder, .{
-        .name = "hello-world",
-        .app = app_mod,
-        .target = target,
-        .optimize = optimize,
-    });
-    b.installArtifact(exe);
+            // Add Mach import to our app.
+            const mach_dep = b.dependency("mach", .{
+                .target = target,
+                .optimize = optimize,
+            });
+            app_mod.addImport("mach", mach_dep.module("mach"));
 
-    // Run the app when `zig build run` is invoked
-    const run_cmd = b.addRunArtifact(exe);
-    run_cmd.step.dependOn(b.getInstallStep());
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
+            // Have Mach create the executable for us
+            const exe = @import("mach").addExecutable(mach_dep.builder, .{
+                .name = "hello-world",
+                .app = app_mod,
+                .target = target,
+                .optimize = optimize,
+            });
+            b.installArtifact(exe);
+
+            // Run the app when `zig build run` is invoked
+            const run_cmd = b.addRunArtifact(exe);
+            run_cmd.step.dependOn(b.getInstallStep());
+            if (b.args) |args| {
+                run_cmd.addArgs(args);
+            }
+            const run_step = b.step(ex.name, "Run the app");
+            run_step.dependOn(&run_cmd.step);
+
+            // // Run tests when `zig build test` is run
+            // const app_unit_tests = b.addTest(.{
+            //     .root_module = app_mod,
+            // });
+            // const run_app_unit_tests = b.addRunArtifact(app_unit_tests);
+            // const test_step = b.step("test", "Run unit tests");
+            // test_step.dependOn(&run_app_unit_tests.step);
+        }
     }
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
-
-    // Run tests when `zig build test` is run
-    const app_unit_tests = b.addTest(.{
-        .root_module = app_mod,
-    });
-    const run_app_unit_tests = b.addRunArtifact(app_unit_tests);
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_app_unit_tests.step);
 }
